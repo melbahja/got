ad9329f9d4cb0b25863a846d3fe95d61d820a1df
Mohamed Elbahja <bm9qdw5r@gmail.com>
2020-08-15 14:19:48 +0100

    cli: handle piped stdin and batch file and multiple download args

--- cmd/got/main.go
+++ cmd/got/main.go
@@ -1,81 +1,111 @@
 package main
 
 import (
-	"flag"
+	"bufio"
+	"context"
+	"errors"
 	"fmt"
 	"log"
+	"net/url"
 	"os"
+	"path/filepath"
+	"runtime"
 	"time"
 
 	"github.com/apoorvam/goterminal"
 	"github.com/dustin/go-humanize"
 	"github.com/melbahja/got"
+	"github.com/urfave/cli/v2"
 )
 
 var (
-	url         string
-	version     string
-	dest        = flag.String("out", "", "Downloaded file destination.")
-	chunkSize   = flag.Uint64("size", 0, "Maximum chunk size in bytes.")
-	concurrency = flag.Uint("concurrency", 10, "Maximum chunks to download at the same time.")
+	version      string
+	errInterrupt = errors.New("Error signal interrupted")
 )
 
-func init() {
+// TODO: handle signals
+// TODO: handle multiple default file names
+func main() {
 
-	flag.Usage = func() {
-		fmt.Println("Got - the fast http downloader.")
-		fmt.Println("\nUsage:\n  got --out path/file.zip http://example.com/file.zip")
-		fmt.Printf("\nVersion:\n  %s\n", version)
-		fmt.Println("\nAuthor:\n  Mohamed Elbahja <bm9qdW5r@gmail.com>")
-		fmt.Println("\nFlags:")
-		flag.PrintDefaults()
+	// New context.
+	ctx, cancel := context.WithCancel(context.Background())
+
+	defer cancel()
+
+	// CLI app.
+	app := &cli.App{
+		Name:  "Got",
+		Usage: "The fastest http downloader.",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "output",
+				Value:   "got.output",
+				Usage:   "Download save path.",
+				Aliases: []string{"out", "o", "save"},
+			},
+			&cli.StringFlag{
+				Name:    "batch",
+				Usage:   "Batch download file from list in file.",
+				Aliases: []string{"bf", "f"},
+			},
+			&cli.Uint64Flag{
+				Name:    "size",
+				Usage:   "File chunks size.",
+				Aliases: []string{"chunk"},
+			},
+			&cli.UintFlag{
+				Name:    "concurrency",
+				Value:   uint(runtime.NumCPU() * 2),
+				Usage:   "Number of chunks to download at the same time.",
+				Aliases: []string{"c"},
+			},
+		},
+		Version: version,
+		Authors: []*cli.Author{
+			{
+				Name:  "Mohamed Elbahja and Contributors",
+				Email: "bm9qdW5r@gmail.com",
+			},
+		},
+		Action: func(c *cli.Context) error {
+
+			if err := run(ctx, c); err != nil {
+				return err
+			}
+
+			fmt.Println("Done!")
+			return nil
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
 	}
 }
 
-func main() {
-
-	flag.Parse()
-
-	if url = flag.Arg(0); url == "" {
-		log.Fatal("Empty download url.")
-	}
-
-	if !(url[:7] == "http://" || url[:8] == "https://") {
-		url = "https://" + url
-	}
-
-	if *dest == "" {
-		*dest = got.GetFilename(url)
-	}
-
-	d := got.Download{
-		URL:         url,
-		Dest:        *dest,
-		ChunkSize:   *chunkSize,
-		Interval:    100,
-		Concurrency: *concurrency,
-	}
-
-	if err := d.Init(); err != nil {
-		log.Fatal(err)
-	}
+func run(ctx context.Context, c *cli.Context) error {
 
 	// Goterm writer.
 	writer := goterminal.New(os.Stdout)
+	defer writer.Reset()
 
-	// Set progress func to update cli output.
-	d.Progress.ProgressFunc = func(p *got.Progress, d *got.Download) {
+	// New *Got.
+	g := got.NewWithContext(ctx)
+
+	// Progress.
+	g.ProgressFunc = func(d *got.Download) {
 
 		writer.Clear()
 
 		fmt.Fprintf(
 			writer,
-			"Downloading: (%s/%s) | Time: %s | Avg: %s/s | Speed: %s/s | Chunk: %s | Concurrency: %d\n",
-			humanize.Bytes(p.Size),
-			humanize.Bytes(p.TotalSize),
-			p.TotalCost().Round(time.Second),
-			humanize.Bytes(p.AvgSpeed()),
-			humanize.Bytes(p.Speed()),
+			"Downloading: %s \nProgress: (%s/%s) | Time: %s | Avg: %s/s | Speed: %s/s | Chunk: %s | Concurrency: %d\n",
+			d.URL,
+			humanize.Bytes(d.Size()),
+			humanize.Bytes(d.TotalSize()),
+			d.TotalCost().Round(time.Second),
+			humanize.Bytes(d.AvgSpeed()),
+			humanize.Bytes(d.Speed()),
 			humanize.Bytes(d.ChunkSize),
 			d.Concurrency,
 		)
@@ -83,10 +113,90 @@ func main() {
 		writer.Print()
 	}
 
-	if err := d.Start(); err != nil {
-		log.Fatal(err)
+	info, err := os.Stdin.Stat()
+
+	if err != nil {
+		return err
 	}
 
-	writer.Reset()
-	fmt.Println("Done!")
+	// Piped stdin
+	if info.Mode()&os.ModeNamedPipe > 0 {
+
+		if err := multiDownload(ctx, c, g, writer, bufio.NewScanner(os.Stdin)); err != nil {
+			return err
+		}
+	}
+
+	// Batch file.
+	if c.String("batch") != "" {
+
+		file, err := os.Open(c.String("batch"))
+
+		if err != nil {
+			return err
+		}
+
+		if err := multiDownload(ctx, c, g, writer, bufio.NewScanner(file)); err != nil {
+			return err
+		}
+	}
+
+	// Download from args.
+	for _, url := range c.Args().Slice() {
+
+		if err = download(ctx, c, g, url); err != nil {
+			return err
+		}
+
+		writer.Reset()
+	}
+
+	return nil
+}
+
+func multiDownload(ctx context.Context, c *cli.Context, g *got.Got, writer *goterminal.Writer, scanner *bufio.Scanner) error {
+
+	for scanner.Scan() {
+
+		if err := download(ctx, c, g, scanner.Text()); err != nil {
+			return err
+		}
+
+		writer.Reset()
+	}
+
+	return nil
+}
+
+func download(ctx context.Context, c *cli.Context, g *got.Got, url string) (err error) {
+
+	if url, err = getURL(url); err != nil {
+		return err
+	}
+
+	// TODO: fix dublicated default filename
+	fname := got.GetFilename(url)
+
+	return g.Do(&got.Download{
+		URL:         url,
+		Dest:        filepath.Join(c.String("output"), fname),
+		ChunkSize:   c.Uint64("size"),
+		Concurrency: c.Uint("concurrency"),
+	})
+}
+
+func getURL(URL string) (string, error) {
+
+	u, err := url.Parse(URL)
+
+	if err != nil {
+		return "", err
+	}
+
+	// Fallback to https by default.
+	if u.Scheme == "" {
+		u.Scheme = "https"
+	}
+
+	return u.String(), nil
 }
--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/apoorvam/goterminal v0.0.0-20180523175556-614d345c47e5
 	github.com/dustin/go-humanize v1.0.0
 	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/urfave/cli/v2 v2.2.0
 	golang.org/x/sync v0.0.0-20200625203802-6e8e738ad208
 )
