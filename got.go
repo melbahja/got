98d591217511c299e192d6ce445e2b37da64ec2e
Mohamed Elbahja <bm9qdw5r@gmail.com>
2020-08-16 00:11:26 +0100

    fmt code.

--- got.go
+++ got.go
@@ -2,40 +2,10 @@ package got
 
 import (
 	"context"
+	"errors"
 	"net/http"
 	"time"
-	"errors"
 )
-
-// DefaulUserAgent is the default Got user agent to send http requests.
-const DefaulUserAgent = "Got/1.0"
-
-// ErrDownloadAborted - When download is aborted by the OS before it is completed, ErrDownloadAborted will be triggered
-var ErrDownloadAborted = errors.New("Operation aborted")
-
-// NewRequest returns a new http.Request and error if any.
-func NewRequest(ctx context.Context, method, URL string) (req *http.Request, err error) {
-
-	if req, err = http.NewRequestWithContext(ctx, method, URL, nil); err != nil {
-		return
-	}
-
-	req.Header.Set("User-Agent", DefaulUserAgent)
-	return
-}
-
-// GetDefaultClient returns Got default http.Client
-func GetDefaultClient() *http.Client {
-
-	return &http.Client{
-		Transport: &http.Transport{
-			MaxIdleConns:        10,
-			IdleConnTimeout:     30 * time.Second,
-			TLSHandshakeTimeout: 5 * time.Second,
-			Proxy:               http.ProxyFromEnvironment,
-		},
-	}
-}
 
 // Got holds got download config.
 type Got struct {
@@ -45,6 +15,12 @@ type Got struct {
 
 	ctx context.Context
 }
+
+// DefaulUserAgent is the default Got user agent to send http requests.
+const DefaulUserAgent = "Got/1.0"
+
+// ErrDownloadAborted - When download is aborted by the OS before it is completed, ErrDownloadAborted will be triggered
+var ErrDownloadAborted = errors.New("Operation aborted")
 
 // Download creates *Download item and runs it.
 func (g Got) Download(URL, dest string) error {
@@ -86,5 +62,29 @@ func NewWithContext(ctx context.Context) *Got {
 	return &Got{
 		ctx:    ctx,
 		Client: GetDefaultClient(),
+	}
+}
+
+// NewRequest returns a new http.Request and error if any.
+func NewRequest(ctx context.Context, method, URL string) (req *http.Request, err error) {
+
+	if req, err = http.NewRequestWithContext(ctx, method, URL, nil); err != nil {
+		return
+	}
+
+	req.Header.Set("User-Agent", DefaulUserAgent)
+	return
+}
+
+// GetDefaultClient returns Got default http.Client
+func GetDefaultClient() *http.Client {
+
+	return &http.Client{
+		Transport: &http.Transport{
+			MaxIdleConns:        10,
+			IdleConnTimeout:     30 * time.Second,
+			TLSHandshakeTimeout: 5 * time.Second,
+			Proxy:               http.ProxyFromEnvironment,
+		},
 	}
 }
