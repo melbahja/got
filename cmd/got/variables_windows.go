65b83a0eb3dc06f9acd446a4454e2e00cef58329
Mohamed Elbahja <bm9qdw5r@gmail.com>
2020-08-30 17:36:02 +0100

    fix: change the width of progress based on terminal size

--- cmd/got/variables_windows.go
+++ cmd/got/variables_windows.go
@@ -6,10 +6,11 @@ import "fmt"
 
 // Windows doesn't handle the block-style very well
 var (
-	simpleProgressStyle = "double-"
-	r, l                = "[", "]"
+	progressStyle = "double-"
+	r, l          = "[", "]"
 )
 
 func color(content ...interface{}) string {
 	return fmt.Sprint(content...)
 }
+
--- go.mod
+++ go.mod
@@ -7,4 +7,5 @@ require (
 	github.com/urfave/cli/v2 v2.2.0
 	gitlab.com/poldi1405/go-ansi v1.1.0
 	gitlab.com/poldi1405/go-indicators v1.0.0
+	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a
 )
