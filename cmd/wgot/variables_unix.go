9982b69705f5dea513ea1d691fc7e788cc6b563f
Mohamed Elbahja <bm9qdw5r@gmail.com>
2021-10-31 22:56:15 +0100

    add new wgot cmd

---
+++ cmd/wgot/variables_unix.go
@@ -0,0 +1,19 @@
+//go:build !windows
+// +build !windows
+
+package main
+
+import (
+	"fmt"
+
+	"gitlab.com/poldi1405/go-ansi"
+)
+
+var (
+	progressStyle = "block"
+	r, l          = "▕", "▏"
+)
+
+func color(content ...interface{}) string {
+	return ansi.Blue(fmt.Sprint(content...))
+}
